6027fa6ba704e65f15906f93f4834b2f4314f207
Kevin Duling <kevin.duling@smarsh.com>
2023-03-08 08:46:42 -0800

    playing with an ArcRotate camera for 3rd Person

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, ArcFollowCamera } from "@babylonjs/core";
 
 class App {
   constructor() {
@@ -16,12 +16,35 @@ class App {
     var engine = new Engine(canvas, true);
     var scene = new Scene(engine);
 
-    var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 3, 5, Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
-    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
     var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
     sphere.position.y = 0.5;
-    var ground: Mesh = MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+
+    var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 3, 5, sphere.position, scene);
+    camera.attachControl(canvas, true);
+    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+
+    var ground: Mesh = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+
+    camera.setTarget(sphere.position);
+    camera.wheelPrecision = 100;
+    camera.minZ = 0.3;
+    camera.collisionRadius = new Vector3(0.5, 0.5, 0.5);
+    camera.lowerRadiusLimit = 2;
+    camera.upperRadiusLimit = 20;
+    // camera.lowerBetaLimit = Math.PI / 2;
+    // camera.upperBetaLimit = Math.PI / 4;
+    console.log("lower beta", camera.lowerBetaLimit);
+    console.log("upper beta", camera.upperBetaLimit);
+    camera.panningSensibility = 0; // disable panning
+    camera.useBouncingBehavior = true; // bounce when reaching camera scrolling limits
+    camera.useAutoRotationBehavior = true; // auto rotate after timeout
+    camera.autoRotationBehavior.idleRotationSpeed = 0.1; // radians per second
+    camera.autoRotationBehavior.idleRotationWaitTime = 60000; // milliseconds
+    camera.autoRotationBehavior.idleRotationSpinupTime = 4000; // milliseconds
+    camera.autoRotationBehavior.zoomStopsAnimation = true;
+    camera.useFramingBehavior = true; // frame in on the bounds of the object
+    camera.framingBehavior.radiusScale = 10; // zoom out a bit - works with camera radius
+    camera.framingBehavior.framingTime = 1000; // milliseconds
 
     window.addEventListener("resize", () => {
       engine.resize();
